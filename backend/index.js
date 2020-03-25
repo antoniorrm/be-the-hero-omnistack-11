d7a656ce229c8e74ced860e794d07e46d37da7fd
antoniorrm <antonioraimundo007@gmail.com>
2020-03-25 01:57:03 -0300

    feature Ongs(create/list) add

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-    return res.json({ evento: 'Go Week 11', aluno: "Antonio R"});
-});
-
-app.listen(3333);
