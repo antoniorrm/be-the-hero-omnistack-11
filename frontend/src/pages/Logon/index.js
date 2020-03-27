7722ea0c47d4d548a516d89b8a9c022b0130c9cd
antoniorrm <antonioraimundo007@gmail.com>
2020-03-26 21:19:42 -0300

    feature login with api add

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -19,7 +19,7 @@ export default function Logon() {
 		try {
 			const response = await api.post("sessions", { id });
 			localStorage.setItem("ongId", id);
-			localStorage.setItem("ongName", response.data.nome);
+			localStorage.setItem("ongName", response.data.name);
 			history.push("/profile");
 		} catch (error) {
 			alert(`Algo deu errado: ${error}`);
